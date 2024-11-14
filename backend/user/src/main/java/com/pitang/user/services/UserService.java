8f16f73a0068856ecdbf318ad7682224d2bf7bcf
Carlos Pereira <c.henrique1309@gmail.com>
2024-11-13 23:17:53 -0300

    Criação da classe UserService.
     Classe responsável por fornecer operações de negócio para o micro serviço Car e implementação do teste unitário do método findAllUsers.

---
+++ backend/user/src/main/java/com/pitang/user/services/UserService.java
@@ -0,0 +1,295 @@
+package com.pitang.user.services;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.pitang.common.dtos.cars.CarDTO;
+import com.pitang.common.dtos.cars.SaveUpdateCarDTO;
+import com.pitang.common.dtos.users.SaveUserDTO;
+import com.pitang.common.dtos.users.UpdateUserDTO;
+import com.pitang.common.dtos.users.UserDTO;
+import com.pitang.common.dtos.users.UserInfoDTO;
+import com.pitang.common.exceptions.CustomException;
+import com.pitang.common.proxies.CarProxy;
+import com.pitang.user.entities.User;
+import com.pitang.user.repositories.UserRepository;
+
+/**
+ * Classe responsável por fornecer operações de negócio para a entidade
+ * {@link User}.
+ */
+@Service
+public class UserService {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private CarProxy carProxy;
+
+	/**
+	 * O método obtem todos os {@link User} cadastratos no banco de dados, se
+	 * existir.
+	 *
+	 * @autor Carlos Pereira
+	 *
+	 * @return {@link UserDTO}
+	 */
+	public List<UserDTO> findAllUsers() {
+		List<User> listUsers = userRepository.findAll();
+		List<UserDTO> listUsersDTO = new ArrayList<UserDTO>();
+
+		for (User user : listUsers) {
+			listUsersDTO.add(convertUserToUserDTO(user));
+		}
+
+		return listUsersDTO;
+	}
+
+	/**
+	 * O método obtem um {@link User} por id cadastrato no banco de dados, se
+	 * existir.
+	 *
+	 * @autor Carlos Pereira
+	 *
+	 * @param id
+	 * @return {@link UserDTO}
+	 */
+	public UserDTO findUserById(Long id) {
+		Optional<User> user = userRepository.findById(id);
+
+		if (user.isPresent()) {
+			return convertUserToUserDTO(user.get());
+		}
+
+		return null;
+	}
+
+	/**
+	 * O método obtem um {@link User} por login cadastrato no banco de dados.
+	 *
+	 * @autor Carlos Pereira
+	 *
+	 * @param id
+	 * @return {@link UserDTO}
+	 */
+	public UserDTO findUserByLogin(String login) {
+		Optional<User> user = userRepository.findByLogin(login);
+
+		if (user.isPresent()) {
+			return convertUserToUserDTO(user.get());
+		}
+
+		return null;
+	}
+
+	/**
+	 * O método obtem um {@link User} por login cadastrato no banco de dados.
+	 *
+	 * @autor Carlos Pereira
+	 *
+	 * @param login
+	 * @return {@link UserInfoDTO}
+	 */
+	public UserInfoDTO findMe(String login) {
+		Optional<User> user = userRepository.findByLogin(login);
+
+		if (user.isPresent()) {
+			List<CarDTO> listCarDTO = carProxy.findAllCarsbyIds(user.get().getCars());
+
+			UserInfoDTO userInfoDTO = new UserInfoDTO(user.get().getFirstName(),
+					user.get().getLastName(), user.get().getEmail(), user.get().getBirthday(), user.get().getLogin(),
+					user.get().getPassword(), user.get().getPhone(), listCarDTO, user.get().getCreatedAt(),
+					user.get().getLastLogin());
+
+			return userInfoDTO;
+		}
+		return null;
+	}
+
+	/**
+	 * Método usado para inserir um {@link User} no banco de dados.
+	 *
+	 * @autor Carlos Pereira
+	 *
+	 * @param saveUserDTO
+	 * @return {@link UserDTO}
+	 */
+	@Transactional
+	public UserDTO insertUser(SaveUserDTO saveUserDTO) {
+		List<Long> idsCars = new ArrayList<Long>();
+		
+		User user = new User(saveUserDTO.getFirstName(), saveUserDTO.getLastName(), saveUserDTO.getEmail(),
+				saveUserDTO.getBirthday(), saveUserDTO.getLogin(), passwordEncoder.encode(saveUserDTO.getPassword()),
+				saveUserDTO.getPhone(), idsCars);
+
+		validateInsertUpdateUser(user);
+		
+		for (CarDTO carDTO : saveUserDTO.getCars()) {
+			SaveUpdateCarDTO newCar = new SaveUpdateCarDTO(carDTO.getYear(), carDTO.getLicensePlate(),
+					carDTO.getModel(), carDTO.getColor());
+			idsCars.add(carProxy.insertCar(newCar).getBody().getId());
+		}
+
+		return convertUserToUserDTO(userRepository.save(user));
+	}
+
+	/**
+	 * Metodo usado para atualizar um {@link User}, se existir.
+	 *
+	 * @autor Carlos Pereira
+	 *
+	 * @param idPedido
+	 * @param usuario
+	 * @return {@link UserDTO}
+	 */
+	public UserDTO updateUser(Long id, UpdateUserDTO updateUserDTO) {
+		Optional<User> user = userRepository.findById(id);
+
+		if (user.isPresent()) {
+			user.get().setFirstName(updateUserDTO.getFirstName());
+			user.get().setLastName(updateUserDTO.getLastName());
+			user.get().setEmail(updateUserDTO.getEmail());
+			user.get().setBirthday(updateUserDTO.getBirthday());
+			user.get().setLogin(updateUserDTO.getLogin());
+			user.get().setPassword(passwordEncoder.encode(updateUserDTO.getPassword()));
+			user.get().setPhone(updateUserDTO.getPhone());
+
+			validateInsertUpdateUser(user.get());
+
+			return convertUserToUserDTO(userRepository.save(user.get()));
+		}
+
+		throw new CustomException("Invalid fields", 4, HttpStatus.BAD_REQUEST);
+	}
+
+	/**
+	 * Método usado para excluir por id um {@link User} no banco de dados, se
+	 * existir.
+	 *
+	 * @autor Carlos Pereira
+	 *
+	 * @param id
+	 */
+	@Transactional
+	public void deleteUser(Long id) {
+		Optional<User> user = userRepository.findById(id);
+
+		if (user.isPresent()) {
+			for (Long idCar : user.get().getCars()) {
+				carProxy.deleteCar(idCar);
+			}
+			
+			userRepository.delete(user.get());
+		}
+	}
+
+	/**
+	 * Método usado para atualizar as datas que o usuario realizou o login no banco
+	 * de dados, se existir.
+	 *
+	 * @autor Carlos Pereira
+	 *
+	 * @param id
+	 */
+	public void updateLastLoginUser(Long id) {
+		Optional<User> user = userRepository.findById(id);
+		if (user.isPresent()) {
+			LocalDateTime localDateTimenow = LocalDateTime.now();
+
+			user.get().setLastLogin(
+					user.get().getCurrentLogin() != null ? user.get().getCurrentLogin() : localDateTimenow);
+			user.get().setCurrentLogin(localDateTimenow);
+
+			userRepository.save(user.get());
+		}
+	}
+	
+	/**
+	 * Método para adicionar um carro no usuario.
+	 *  
+	 * @autor Carlos Pereira
+	 * 
+	 * @param id
+	 * @param login
+	 */
+	public void addCarToUser(Long id, String login) {
+		Optional<User> user = userRepository.findByLogin(login);
+
+		if (user.isPresent()) {
+			user.get().getCars().add(id);
+			userRepository.save(user.get());
+		}
+	}
+	
+	/**
+	 * Método para deletar um carro no usuario.
+	 *  
+	 * @autor Carlos Pereira
+	 * 
+	 * @param id
+	 * @param login
+	 */
+	public void deleteCarToUser(Long id, String login) {
+		Optional<User> user = userRepository.findByLogin(login);
+
+		if (user.isPresent()) {
+			user.get().getCars().remove(id);
+			userRepository.save(user.get());
+		}
+	}
+
+	/**
+	 * Método usado para converter um {@link User} em um {@link UserDTO}.
+	 * 
+	 * @autor Carlos Pereira
+	 * 
+	 * @param user
+	 * @return {@link UserDTO}
+	 */
+	private UserDTO convertUserToUserDTO(User user) {
+		List<CarDTO> listCarDTO = new ArrayList<CarDTO>();
+		
+		if(user.getCars() != null && !user.getCars().isEmpty()) {
+			listCarDTO = carProxy.findAllCarsbyIds(user.getCars());			
+		}
+
+		UserDTO userDTO = new UserDTO(user.getId(), user.getFirstName(), user.getLastName(), user.getEmail(),
+				user.getBirthday(), user.getLogin(), user.getPassword(), user.getPhone(), listCarDTO);
+
+		return userDTO;
+	}
+
+	/**
+	 * Método usado para validar inserts e updates do usuario.
+	 * 
+	 * @param user
+	 */
+	private void validateInsertUpdateUser(User user) {
+		if (!user.isValid()) {
+			throw new CustomException("Missing fields", 5, HttpStatus.BAD_REQUEST);
+		}
+
+		Optional<User> userEmail = userRepository.findByEmail(user.getEmail());
+		if (userEmail.isPresent() && !userEmail.get().equals(user)) {
+			throw new CustomException("Email already exists", 2, HttpStatus.CONFLICT);
+		}
+
+		Optional<User> userLogin = userRepository.findByLogin(user.getLogin());
+		if (userLogin.isPresent() && !userLogin.get().equals(user)) {
+			throw new CustomException("Login already exists", 3, HttpStatus.CONFLICT);
+		}
+	}
+
+}
---
+++ backend/user/src/test/java/com/pitang/user/services/UserServiceTest.java
@@ -0,0 +1,117 @@
+package com.pitang.user.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.pitang.common.dtos.cars.CarDTO;
+import com.pitang.common.dtos.users.UserDTO;
+import com.pitang.common.proxies.CarProxy;
+import com.pitang.user.entities.User;
+import com.pitang.user.repositories.UserRepository;
+
+public class UserServiceTest {
+	@Mock
+	private UserRepository userRepository;
+
+	@Mock
+	private CarProxy carProxy;
+
+	@InjectMocks
+	private UserService userService;
+
+	private List<User> mockUsers;
+
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+
+		List<Long> carros1 = Arrays.asList(1L, 2L);
+		List<Long> carros2 = Arrays.asList(3L);
+
+		User user1 = new User(1L, "João", "Silva", "joao.silva@gmail.com", new Date(), "joaosilva", "senha123",
+				"1234567890", carros1);
+		User user2 = new User(2L, "Maria", "Oliveira", "maria.oliveira@gmail.com", new Date(), "mariaoliveira",
+				"senha123", "0987654321", carros2);
+
+		mockUsers = new ArrayList<>();
+		mockUsers.add(user1);
+		mockUsers.add(user2);
+
+		when(userRepository.findAll()).thenReturn(mockUsers);
+
+		when(carProxy.findAllCarsbyIds(Arrays.asList(1L, 2L)))
+				.thenReturn(Arrays.asList(new CarDTO(1L, 2020, "ABC-1234", "ONIX", "PRETO"),
+						new CarDTO(2L, 2021, "XYZ-5678", "YARIS", "BRANCO")));
+
+		when(carProxy.findAllCarsbyIds(Arrays.asList(3L)))
+				.thenReturn(Arrays.asList(new CarDTO(3L, 2022, "YUI-9101", "KICKS", "PRATA")));
+	}
+
+	@Test
+	void testFindAllUsers() {
+		List<UserDTO> result = userService.findAllUsers();
+
+		assertNotNull(result);
+		assertEquals(2, result.size());
+
+		UserDTO userDTO1 = result.get(0);
+		assertEquals(1L, userDTO1.getId());
+		assertEquals("João", userDTO1.getFirstName());
+		assertEquals("Silva", userDTO1.getLastName());
+		assertEquals("joao.silva@gmail.com", userDTO1.getEmail());
+		assertEquals("joaosilva", userDTO1.getLogin());
+		assertEquals("1234567890", userDTO1.getPhone());
+		assertEquals(2, userDTO1.getCars().size());
+		
+		assertEquals(1L, userDTO1.getCars().get(0).getId());
+		assertEquals(2020, userDTO1.getCars().get(0).getYear());
+		assertEquals("ABC-1234", userDTO1.getCars().get(0).getLicensePlate());
+		assertEquals("ONIX", userDTO1.getCars().get(0).getModel());
+		assertEquals("PRETO", userDTO1.getCars().get(0).getColor());
+		
+		assertEquals(2L, userDTO1.getCars().get(1).getId());
+		assertEquals(2021, userDTO1.getCars().get(1).getYear());
+		assertEquals("XYZ-5678", userDTO1.getCars().get(1).getLicensePlate());
+		assertEquals("YARIS", userDTO1.getCars().get(1).getModel());
+		assertEquals("BRANCO", userDTO1.getCars().get(1).getColor());
+
+		UserDTO userDTO2 = result.get(1);
+		assertEquals(2L, userDTO2.getId());
+		assertEquals("Maria", userDTO2.getFirstName());
+		assertEquals("Oliveira", userDTO2.getLastName());
+		assertEquals("maria.oliveira@gmail.com", userDTO2.getEmail());
+		assertEquals("mariaoliveira", userDTO2.getLogin());
+		assertEquals("0987654321", userDTO2.getPhone());
+		assertEquals(1, userDTO2.getCars().size());	
+		
+		assertEquals(3L, userDTO2.getCars().get(0).getId());
+		assertEquals(2022, userDTO2.getCars().get(0).getYear());
+		assertEquals("YUI-9101", userDTO2.getCars().get(0).getLicensePlate());
+		assertEquals("KICKS", userDTO2.getCars().get(0).getModel());
+		assertEquals("PRATA", userDTO2.getCars().get(0).getColor());
+	}
+
+	@Test
+	void testFindAllUsers_EmptyList() {
+		when(userRepository.findAll()).thenReturn(Collections.emptyList());
+
+		List<UserDTO> result = userService.findAllUsers();
+
+		assertNotNull(result);
+		assertTrue(result.isEmpty());
+	}
+}
