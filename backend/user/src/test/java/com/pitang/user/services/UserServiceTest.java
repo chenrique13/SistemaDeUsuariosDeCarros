42468c1d529f10a3a2e57a48b938bc59ab381fe2
Carlos Pereira <c.henrique1309@gmail.com>
2024-11-16 21:42:39 -0300

    Atualização na classe UserServiceTest
    Reestruturação dos métodos.

--- backend/user/src/test/java/com/pitang/user/services/UserServiceTest.java
+++ backend/user/src/test/java/com/pitang/user/services/UserServiceTest.java
@@ -40,21 +40,21 @@ public class UserServiceTest {
 	private UserService userService;
 
 	private List<User> mockUsers;
-
+	
+	private List<Long> car1 = Arrays.asList(1L, 2L);
+	private List<Long> car2 = Arrays.asList(3L);
+	
+	private User user1 = new User(1L, "João", "Silva", "joao.silva@gmail.com", new Date(), "joaosilva", "senha123",
+			"1234567890", car1);
+	private User user2 = new User(2L, "Maria", "Oliveira", "maria.oliveira@gmail.com", new Date(), "mariaoliveira",
+			"senha123", "0987654321", car2);
+	
 	/**
 	 * Inicializa os objetos necessarios antes de cada teste.
 	 */
 	@BeforeEach
 	void runBeforeEachTest() {
 		MockitoAnnotations.openMocks(this);
-
-		List<Long> carros1 = Arrays.asList(1L, 2L);
-		List<Long> carros2 = Arrays.asList(3L);
-
-		User user1 = new User(1L, "João", "Silva", "joao.silva@gmail.com", new Date(), "joaosilva", "senha123",
-				"1234567890", carros1);
-		User user2 = new User(2L, "Maria", "Oliveira", "maria.oliveira@gmail.com", new Date(), "mariaoliveira",
-				"senha123", "0987654321", carros2);
 
 		mockUsers = new ArrayList<>();
 		mockUsers.add(user1);
@@ -64,11 +64,11 @@ public class UserServiceTest {
 		when(userRepository.findById(user1.getId())).thenReturn(Optional.of(user1));
 		when(userRepository.findByLogin(user1.getLogin())).thenReturn(Optional.of(user1));
 
-		when(carProxy.findAllCarsbyIds(Arrays.asList(1L, 2L)))
+		when(carProxy.findAllCarsbyIds(car1))
 				.thenReturn(Arrays.asList(new CarDTO(1L, 2020, "ABC-1234", "ONIX", "PRETO"),
 						new CarDTO(2L, 2021, "XYZ-5678", "YARIS", "BRANCO")));
 
-		when(carProxy.findAllCarsbyIds(Arrays.asList(3L)))
+		when(carProxy.findAllCarsbyIds(car2))
 				.thenReturn(Arrays.asList(new CarDTO(3L, 2022, "YUI-9101", "KICKS", "PRATA")));
 	}
 
@@ -80,13 +80,14 @@ public class UserServiceTest {
 		assertEquals(2, result.size());
 
 		UserDTO userDTO1 = result.get(0);
-		assertEquals(1L, userDTO1.getId());
-		assertEquals("João", userDTO1.getFirstName());
-		assertEquals("Silva", userDTO1.getLastName());
-		assertEquals("joao.silva@gmail.com", userDTO1.getEmail());
-		assertEquals("joaosilva", userDTO1.getLogin());
-		assertEquals("1234567890", userDTO1.getPhone());
-		assertEquals(2, userDTO1.getCars().size());
+		assertEquals(user1.getId(), userDTO1.getId());
+		assertEquals(user1.getFirstName(), userDTO1.getFirstName());
+		assertEquals(user1.getLastName(), userDTO1.getLastName());
+		assertEquals(user1.getEmail(), userDTO1.getEmail());
+		assertEquals(user1.getLogin(), userDTO1.getLogin());
+		assertEquals(user1.getPassword(), userDTO1.getPassword());
+		assertEquals(user1.getPhone(), userDTO1.getPhone());
+		assertEquals(user1.getCars().size(), userDTO1.getCars().size());
 
 		assertEquals(1L, userDTO1.getCars().get(0).getId());
 		assertEquals(2020, userDTO1.getCars().get(0).getYear());
@@ -101,13 +102,14 @@ public class UserServiceTest {
 		assertEquals("BRANCO", userDTO1.getCars().get(1).getColor());
 
 		UserDTO userDTO2 = result.get(1);
-		assertEquals(2L, userDTO2.getId());
-		assertEquals("Maria", userDTO2.getFirstName());
-		assertEquals("Oliveira", userDTO2.getLastName());
-		assertEquals("maria.oliveira@gmail.com", userDTO2.getEmail());
-		assertEquals("mariaoliveira", userDTO2.getLogin());
-		assertEquals("0987654321", userDTO2.getPhone());
-		assertEquals(1, userDTO2.getCars().size());
+		assertEquals(user2.getId(), userDTO2.getId());
+		assertEquals(user2.getFirstName(), userDTO2.getFirstName());
+		assertEquals(user2.getLastName(), userDTO2.getLastName());
+		assertEquals(user2.getEmail(), userDTO2.getEmail());
+		assertEquals(user2.getLogin(), userDTO2.getLogin());
+		assertEquals(user2.getPassword(), userDTO2.getPassword());
+		assertEquals(user2.getPhone(), userDTO2.getPhone());
+		assertEquals(user2.getCars().size(), userDTO2.getCars().size());
 
 		assertEquals(3L, userDTO2.getCars().get(0).getId());
 		assertEquals(2022, userDTO2.getCars().get(0).getYear());
@@ -128,59 +130,58 @@ public class UserServiceTest {
 
 	@Test
 	void testFindUserById_UserExists() {
-		Long userId = 1L;
-		UserDTO result = userService.findUserById(userId);
+		UserDTO result = userService.findUserById(user1.getId());
 
 		assertNotNull(result);
-		assertEquals(userId, result.getId());
-		assertEquals("João", result.getFirstName());
-		assertEquals("Silva", result.getLastName());
-		assertEquals("joao.silva@gmail.com", result.getEmail());
-		assertEquals("joaosilva", result.getLogin());
-		assertEquals("1234567890", result.getPhone());
-		assertEquals(2, result.getCars().size());
+		assertEquals(user1.getId(), result.getId());
+		assertEquals(user1.getFirstName(), result.getFirstName());
+		assertEquals(user1.getLastName(), result.getLastName());
+		assertEquals(user1.getEmail(), result.getEmail());
+		assertEquals(user1.getLogin(), result.getLogin());
+		assertEquals(user1.getPassword(), result.getPassword());
+		assertEquals(user1.getPhone(), result.getPhone());
+		assertEquals(user1.getCars().size(), result.getCars().size());
 
-		verify(userRepository).findById(userId);
+		verify(userRepository).findById(user1.getId());
 	}
 
 	@Test
 	void testFindUserById_UserDoesNotExist() {
-		Long userId = 1L;
-		when(userRepository.findById(userId)).thenReturn(Optional.empty());
+		when(userRepository.findById(user1.getId())).thenReturn(Optional.empty());
 
-		UserDTO result = userService.findUserById(userId);
+		UserDTO result = userService.findUserById(user1.getId());
 
 		assertNull(result);
 
-		verify(userRepository).findById(userId);
+		verify(userRepository).findById(user1.getId());
 	}
 	
 	@Test
 	void testFindUserByLogin_UserExists() {
-		String userLogin = "joaosilva";
-		UserDTO result = userService.findUserByLogin(userLogin);
+		UserDTO result = userService.findUserByLogin(user1.getLogin());
 
 		assertNotNull(result);
-		assertEquals(1L, result.getId());
-		assertEquals("João", result.getFirstName());
-		assertEquals("Silva", result.getLastName());
-		assertEquals("joao.silva@gmail.com", result.getEmail());
-		assertEquals(userLogin, result.getLogin());
-		assertEquals("1234567890", result.getPhone());
-		assertEquals(2, result.getCars().size());
+		assertEquals(user1.getId(), result.getId());
+		assertEquals(user1.getFirstName(), result.getFirstName());
+		assertEquals(user1.getLastName(), result.getLastName());
+		assertEquals(user1.getEmail(), result.getEmail());
+		assertEquals(user1.getLogin(), result.getLogin());
+		assertEquals(user1.getPassword(), result.getPassword());
+		assertEquals(user1.getPhone(), result.getPhone());
+		assertEquals(user1.getCars().size(), result.getCars().size());
 
-		verify(userRepository).findByLogin(userLogin);
+		verify(userRepository).findByLogin(user1.getLogin());
 	}
 	
 	@Test
 	void testFindUserByLogin_UserDoesNotExist() {
-		String userLogin = "joaosilva";
-		when(userRepository.findByLogin(userLogin)).thenReturn(Optional.empty());
+		when(userRepository.findByLogin(user1.getLogin())).thenReturn(Optional.empty());
 
-		UserDTO result = userService.findUserByLogin(userLogin);
+		UserDTO result = userService.findUserByLogin(user1.getLogin());
 
 		assertNull(result);
 
-		verify(userRepository).findByLogin(userLogin);
+		verify(userRepository).findByLogin(user1.getLogin());
+	}
 	}
 }
